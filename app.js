3da7c462a273ed6243b27671a3967c328e7cefc8
Cassie Andrews <candrews1@ufl.edu>
2025-01-30 21:15:03 -0500

    API key, initial edits

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 const app = express()
 // TODO: import the getCityInfo and getJobs functions from util.js
 
 // TODO: Statically serve the public folder
-
+app.use(express.static('public'))
 // TODO: declare the GET route /api/city/:city
-// This endpoint should call getCityInfo and getJobs and return
-// the result as JSON.
+app.get('/api/city/:city', (req, res) => {
+    res.sendFile(path.join(__dirname, '/api/city/:city'))
+    // This endpoint should call getCityInfo and getJobs and return
+    // the result as JSON.
+})
+
+
 // The returned JSON object should have two keys:
 // cityInfo (with value of the getCityInfo function)
 // jobs (with value of the getJobs function)
+
 // If no city info or jobs are found,
 // the endpoint should return a 404 status
 
