e50662fcd1be83800be25fdd06196eb6a251e8f5
Cassie Andrews <candrews1@ufl.edu>
2025-02-01 16:43:12 -0500

    app functionality

--- app.js
+++ app.js
@@ -2,23 +2,36 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const app = express()
-// TODO: import the getCityInfo and getJobs functions from util.js
+const port = 3000;
+const { getJobs, getCityInfo } = require('./util.js') // import getCityInfo and getJobs functions from util.js
 
-// TODO: Statically serve the public folder
-app.use(express.static('public'))
-// TODO: declare the GET route /api/city/:city
-app.get('/api/city/:city', (req, res) => {
-    res.sendFile(path.join(__dirname, '/api/city/:city'))
-    // This endpoint should call getCityInfo and getJobs and return
-    // the result as JSON.
+app.use(express.static('public')) // Statically serve the public folder
+app.get('/api/city/:city', async (req, res) => { // declare the GET route /api/city/:city
+    const city = req.params.city;
+
+    try {
+        // This endpoint should call getCityInfo and getJobs
+        const cityInfo = await getCityInfo(city);
+        const jobs = await getJobs(city);
+        
+        // If no city info or jobs are found,
+        // the endpoint should return a 404 status
+        if (!cityInfo || !jobs) {
+            return res.status(404).json({
+                success: false,
+                error: "No results found"
+            })
+        }
+        // If city info or jobs are found, return the result as JSON.
+        res.json({
+        // The returned JSON object should have two keys:
+            cityInfo,// cityInfo (with value of the getCityInfo function)
+            jobs// jobs (with value of the getJobs function)
+    })
+    } catch (error) {
+        console.log(error);
+
+    }
 })
-
-
-// The returned JSON object should have two keys:
-// cityInfo (with value of the getCityInfo function)
-// jobs (with value of the getJobs function)
-
-// If no city info or jobs are found,
-// the endpoint should return a 404 status
 
 module.exports = app
